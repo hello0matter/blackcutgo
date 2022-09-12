3a10ac2a81369acd570ecde05804e545a25f52fb
xiaoxiong <1066562980@qq.com>
2022-09-13 02:55:15 +0800

    feat: 定制抢购panicBuying

--- blackgo/blackgo.py
+++ blackgo/blackgo.py
@@ -1,43 +1,47 @@
 # coding=utf-8
 import os
-import os.path as osp
 import json
-import datetime as datetime
-from git.repo import Repo
-from datetime import datetime, date, timedelta
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-def Task():
-    now = datetime.datetime.now()
-    ts = now.strftime('%Y-%m-%d %H:%M:%S')
-    print("time run by ",ts)
-
-def APschedulerMonitor():
-    # 创建调度器：BlockingScheduler
-    scheduler = BlockingScheduler()
-    scheduler.add_job(Task, 'interval', seconds=0.1, id='test_job1')
-    # 添加任务,时间间隔5S
-    scheduler.add_job(Task, 'interval', seconds=0.1, id='test_job2')
-    scheduler.start()
+from datetime import datetime
+import timetask
+import subprocess
 
 class JsonRead:
     def __init__(self, d):
         self.__dict__ = d
 
-download_path = os.path.join('store')
-print("now time",datetime.now())
 
 with open('./blackgo.json', 'r', encoding='utf-8') as f:
     jsonStr = f.read()
-    json = json.loads(jsonStr, object_hook=JsonRead)
-    if json.gitParameter:
-        print(datetime.strptime(json.gitParameter.time, "%Y-%m-%d %H:%M:%S"))
-        # new_repo = Repo.clone_from(url='git@github.com:gcsdfaksh/blackcutgo.git', to_path=download_path)
-        # repo = Repo.clone_from('https://github.com/gcsdfaksh/blackcutgo.git',
-        #                            osp.join(download_path), branch='master')
-        if ~os.path.exists(download_path):
-            os.mkdir(download_path)
-        os.chdir(download_path)
-    else:
-        print("没有git配置，程序退出")
+    config = json.loads(jsonStr, object_hook=JsonRead)
+    if "gitParameter" in dir(config):
+        if config.gitParameter.timedelta:
+            timetask.git_monitor(config.gitParameter.timedelta)
+        else:
+            print("未设置git 拉取时间 timedelta") / exit()
+        if "panicBuying" in dir(config):
+            buy = config.panicBuying
+            if len(list(buy)) == 0:
+                print("抢购包数据条数为0") / exit()
+            for item in buy:
+                name = item.name if "name" in dir(item) else print("name不存在") / exit()
+                if item.enable:
+                    runscript = "python " + name + "/" + name + ".py --time \"" + item.time + "\" --type " + str(
+                        item.type) + " --desc " + item.desc if "desc" in dir(item) else ""
+                    message = os.popen(runscript)
+                    print(message.read())
 
+                    # res = subprocess.Popen(runscript, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) # 使用管道
+                    # print(res.stdout.read())  # 标准输出
+                    # for line in res.stdout.readlines():
+                    #     print(line)
+                    # res.stdout.close()         # 关闭
+                    print("执行" + name + "软件包")
+                else:
+                    print(item.name, "已经关闭")
+                # os.system("pause")
+                input()
+            else:
+                print("没有git配置，程序退出")
+    f.close()
+
+print("now time", datetime.now())
---
+++ blackgo/hulianar/hulianar.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+import os
+import sys
+import blackgo.timetask as timetask
+import argparse
+import json
+
+
+class JsonRead:
+    def __init__(self, d):
+        self.__dict__ = d
+
+
+# def post(exe, thread):
+#     os.popen(exe)
+def post():
+    print(1)
+    pass
+
+
+parser = argparse.ArgumentParser()
+
+# 在参数构造器中添加两个命令行参数
+parser.add_argument('--time', type=str, default='')
+parser.add_argument('--type', type=int, default=1)
+parser.add_argument('--desc', type=str, default='')
+
+# 获取所有的命令行参数
+args = parser.parse_args()
+with open(sys.path[0] + '/account.txt', 'r', encoding='utf-8') as f:
+    read_data = f.readlines()
+    if args.desc:
+        print(args.desc, "长度", len(read_data))
+
+    for item in read_data:
+        txt = item.split("----")
+        print(txt)
+    f.closed
+
+    # timetask.buy_monitor("* * * * * *", post, "hulianar")
+    with open(sys.path[0] + '/conf.json', 'r', encoding='utf-8') as f:
+        jsonStr = f.read()
+        config = json.loads(jsonStr, object_hook=JsonRead)
+
+        if "curl" in dir(config):
+            this_cut = config.curl
+            # 替换参数
+            this_cut.replace('${1}', "")
+            this_cut.replace('${2}', "")
+        else:
+            print("未设置curl语句")
+
+        this_cut = """while true
+        do
+            """ + this_cut + """
+        done"""
+
+    for x in range(int(config.thread)):
+        # timetask.buy_monitor(args.time, post(exe=this_cut, thread=config.thread), "hulianar" + str(x)) if args.time else print(
+        #     "未设置执行时间") / exit()
+        timetask.buy_monitor(args.time, post, "hulianar" + str(x)) if args.time else print("未设置执行时间") / exit()
+
+    os.system("pause")
