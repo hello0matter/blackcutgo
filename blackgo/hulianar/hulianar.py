48d741b8b8b58dab571e71898e99dcbab02be1bf
xiaoxiong <1066562980@qq.com>
2022-09-19 21:58:48 +0800

    feat: 添加一个python版本更新脚本 ,将venv上传

--- blackgo/hulianar/hulianar.py
+++ blackgo/hulianar/hulianar.py
@@ -5,7 +5,7 @@ sys.path.insert(1, '../')
 import blackgo.timetask as timetask
 import argparse
 import json
-
+import requests
 
 class JsonRead:
     def __init__(self, d):
@@ -14,7 +14,15 @@ class JsonRead:
 
 def post(exe, thread):
     print("run with", sys.path[0] + '/conf.json')
-    os.popen(exe)
+    url = 'https://services.mojing1.com/api/Mall/list?page=1&limit=10&sort=price%2Ctime&order=desc%2Cdesc&keywords=&id&isMystery=0&rights=-1&level'
+    headers = {'Host': 'services.mojing1.com', 'Connection': 'keep-alive', 'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"', 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjYwOTIzMTAsImlhdCI6MTY2MzUwMDMxMCwidXNlcklkIjoiNzA2MzQifQ.QSOxQe-vlvmjc87HaDqBDlX-XWoJMwBRpFme_1fsVeo', 'sec-ch-ua-mobile': '?0', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42', 'sec-ch-ua-platform': '"Windows"', 'Accept': '*/*', 'Origin': 'https://app.mojing1.com', 'Sec-Fetch-Site': 'same-site', 'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Dest': 'empty', 'Referer': 'https://app.mojing1.com/', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6'}
+    cookies = {}
+    data = {}
+
+    html = requests.get(url, headers=headers, verify=False, cookies=cookies)
+    print(len(html.text))
+    print(html.text)
+    # os.popen(exe)
 
 
 parser = argparse.ArgumentParser()
@@ -40,6 +48,16 @@ with open(sys.path[0] + '/account.txt', 'r', encoding='utf-8') as f:
     with open(sys.path[0] + '/conf.json', 'r', encoding='utf-8') as f:
         jsonStr = f.read()
         config = json.loads(jsonStr, object_hook=JsonRead)
+
+
+        url = 'https://services.mojing1.com/api/Mall/list?page=1&limit=10&sort=price%2Ctime&order=desc%2Cdesc&keywords=&id&isMystery=0&rights=-1&level'
+        headers = {'Host': 'services.mojing1.com', 'Connection': 'keep-alive', 'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"', 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjYwOTIzMTAsImlhdCI6MTY2MzUwMDMxMCwidXNlcklkIjoiNzA2MzQifQ.QSOxQe-vlvmjc87HaDqBDlX-XWoJMwBRpFme_1fsVeo', 'sec-ch-ua-mobile': '?0', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42', 'sec-ch-ua-platform': '"Windows"', 'Accept': '*/*', 'Origin': 'https://app.mojing1.com', 'Sec-Fetch-Site': 'same-site', 'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Dest': 'empty', 'Referer': 'https://app.mojing1.com/', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6'}
+        cookies = {}
+        data = {}
+
+        html = requests.get(url, headers=headers, verify=False, cookies=cookies)
+        print(len(html.text))
+        print(html.text)
 
         if "curl" in dir(config):
             this_cut = config.curl
