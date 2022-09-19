64e05f84db624e754724034b1bf7c099b37c93c9
xiaoxiong <1066562980@qq.com>
2022-09-19 22:25:57 +0800

    feat: 添加一个python版本更新脚本 ,将venv上传

--- blackgo/hulianar/hulianar.py
+++ blackgo/hulianar/hulianar.py
@@ -12,17 +12,11 @@ class JsonRead:
         self.__dict__ = d
 
 
-def post(exe, thread):
-    print("run with", sys.path[0] + '/conf.json')
-    url = 'https://services.mojing1.com/api/Mall/list?page=1&limit=10&sort=price%2Ctime&order=desc%2Cdesc&keywords=&id&isMystery=0&rights=-1&level'
-    headers = {'Host': 'services.mojing1.com', 'Connection': 'keep-alive', 'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"', 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjYwOTIzMTAsImlhdCI6MTY2MzUwMDMxMCwidXNlcklkIjoiNzA2MzQifQ.QSOxQe-vlvmjc87HaDqBDlX-XWoJMwBRpFme_1fsVeo', 'sec-ch-ua-mobile': '?0', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42', 'sec-ch-ua-platform': '"Windows"', 'Accept': '*/*', 'Origin': 'https://app.mojing1.com', 'Sec-Fetch-Site': 'same-site', 'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Dest': 'empty', 'Referer': 'https://app.mojing1.com/', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6'}
-    cookies = {}
-    data = {}
+def post(thread, frequency, url, headers, cookies, data):
 
     html = requests.get(url, headers=headers, verify=False, cookies=cookies)
     print(len(html.text))
     print(html.text)
-    # os.popen(exe)
 
 
 parser = argparse.ArgumentParser()
@@ -49,30 +43,15 @@ with open(sys.path[0] + '/account.txt', 'r', encoding='utf-8') as f:
         jsonStr = f.read()
         config = json.loads(jsonStr, object_hook=JsonRead)
 
-
         url = 'https://services.mojing1.com/api/Mall/list?page=1&limit=10&sort=price%2Ctime&order=desc%2Cdesc&keywords=&id&isMystery=0&rights=-1&level'
         headers = {'Host': 'services.mojing1.com', 'Connection': 'keep-alive', 'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"', 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjYwOTIzMTAsImlhdCI6MTY2MzUwMDMxMCwidXNlcklkIjoiNzA2MzQifQ.QSOxQe-vlvmjc87HaDqBDlX-XWoJMwBRpFme_1fsVeo', 'sec-ch-ua-mobile': '?0', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42', 'sec-ch-ua-platform': '"Windows"', 'Accept': '*/*', 'Origin': 'https://app.mojing1.com', 'Sec-Fetch-Site': 'same-site', 'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Dest': 'empty', 'Referer': 'https://app.mojing1.com/', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6'}
         cookies = {}
         data = {}
 
-        html = requests.get(url, headers=headers, verify=False, cookies=cookies)
-        print(len(html.text))
-        print(html.text)
-
-        if "curl" in dir(config):
-            this_cut = config.curl
-            # 替换参数
-            this_cut.replace('${1}', "")
-            this_cut.replace('${2}', "")
-        else:
-            print("未设置curl语句")
-
-        this_cut = "while true\r\ndo\r\n" + this_cut +"\r\ndone"
-
-        print("线程数", config.thread, this_cut)
+        print("线程数", config.thread)
 
         for x in range(config.thread):
             # timetask.buy_monitor(args.time, post, "hulianar" + str(x)) if args.time else print("未设置执行时间") / exit()
-            timetask.buy_monitor(args.time, post, "hulianar" + str(x), dict(exe=this_cut, thread=config.thread)) if args.time else print("未设置执行时间") / exit()
+            timetask.buy_monitor(args.time, post, "hulianar" + str(x), dict(thread=config.thread, url=url, headers=headers, cookies=cookies, data=data)) if args.time else print("未设置执行时间") / exit()
 
-        # os.system("pause")
+        input()
