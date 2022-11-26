b1ab9152f7b79d699e4bc152a4b73c8197db547f
gcsdfaksh <1066562980@qq.com>
2022-11-26 19:04:08 +0800

    fix: 安卓自动测试

--- blackgo_tool/repeat_scan_html_tool/repeat_scan_html_tool/spiders/test.py
+++ blackgo_tool/repeat_scan_html_tool/repeat_scan_html_tool/spiders/test.py
@@ -49,7 +49,7 @@ class TestSpidertype2(scrapy.Spider):
 class TestSpidertype1(scrapy.Spider):
     name = 'testtype1'
     # allowed_domains = ['www.pzcode.cn']
-    num = 60064300
+    num = 71273078
     urltype = 'https://www.pzcode.cn/Manage/Basic/Battery/ScanView.aspx?type=1'
     start_urls = [urltype + '&id=' + str(num)]
 
