611cc9ca828769c2243c22c3581b4ba45aee772a
gcsdfaksh <1066562980@qq.com>
2023-02-02 10:47:17 +0800

    fix: 去除错误卡死

--- blackgo_gui/qt__source/main.py
+++ blackgo_gui/qt__source/main.py
@@ -531,10 +531,10 @@ def querys():
                         all.flush()
                         b = b + 1
 
-                        if not refail:
-                            jpg_ = open2text + "/" + ' ' + split + ".png"
-                            jpg_split = open2text + "/" + split + ".png"
-                            jpg_2 = open2text + "/error/" + ' ' + split + ".png"
+                        # if not refail:
+                        jpg_ = open2text + "/" + ' ' + split + ".png"
+                        jpg_split = open2text + "/" + split + ".png"
+                        jpg_2 = open2text + "/error/" + ' ' + split + ".png"
                         if re['msg'] == "绑定成功" or re['code'] == 0:
 
                             L = len(split)
